f40fc3ec25712d4ca225c3c036786546bcf8b21c
IvanGuculyak <ivan.guculyak@gmail.com>
2023-10-25 23:34:11 +0300

    Added to the admin page and registered user model with user manager

--- user/admin.py
+++ user/admin.py
@@ -1,19 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import gettext as _
 
-from theatre_service.models import (
-    TheatreHall,
-    Genre,
-    Actor,
-    Play,
-    Performance,
-    Reservation,
-    Ticket,
-)
+from .models import User
 
-admin.site.register(TheatreHall)
-admin.site.register(Genre)
-admin.site.register(Actor)
-admin.site.register(Play)
-admin.site.register(Performance)
-admin.site.register(Reservation)
-admin.site.register(Ticket)
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    """Define admin model for custom User model with no email field."""
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
