6ded6681e215c1b7f4ee10a9452be8271526ac2b
IvanGuculyak <ivan.guculyak@gmail.com>
2023-10-27 15:00:07 +0300

    Added docker media volumes and rights for django-user

--- theatre_service/urls.py
+++ theatre_service/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework import routers
 
@@ -15,9 +17,11 @@ router.register("genres", GenreViewSet)
 router.register("actors", ActorViewSet)
 router.register("theatre_halls", TheatreHallViewSet)
 router.register("plays", PlayViewSet)
-router.register("Performance", PerformanceViewSet)
+router.register("performance", PerformanceViewSet)
 router.register("reservation", ReservationViewSet)
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("", include(router.urls)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 app_name = "theatre_service"
